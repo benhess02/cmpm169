4e5fe8318d4fa97326a6bd23488d158589cffbd2
Ben Hess <pilotben@comcast.net>
2025-02-03 21:41:15 -0800

    Added experiment 4

--- experiment3/js/sketch.js
+++ experiment3/js/sketch.js
@@ -1,79 +1,81 @@
-// sketch.js - purpose and description here
-// Author: Your Name
-// Date:
+let img1;
+let img2;
+let img;
 
-// Here is how you might set up an OOP p5.js project
-// Note that p5.js looks for a file called sketch.js
+let index = 0;
 
-// Constants - User-servicable parts
-// In a longer project I like to put these in a separate file
-const VALUE1 = 1;
-const VALUE2 = 2;
+let amplitudes0 = [];
+let amplitudes2 = [];
 
-// Globals
-let myInstance;
-let canvasContainer;
-var centerHorz, centerVert;
+let oscillators = []
+let oscillators2 = []
 
-class MyClass {
-    constructor(param1, param2) {
-        this.property1 = param1;
-        this.property2 = param2;
-    }
-
-    myMethod() {
-        // code to run when method is called
-    }
+function preload() {
+  img = loadImage("/Houses_of_Parliament.jpg");
 }
 
-function resizeScreen() {
-  centerHorz = canvasContainer.width() / 2; // Adjusted for drawing logic
-  centerVert = canvasContainer.height() / 2; // Adjusted for drawing logic
-  console.log("Resizing...");
-  resizeCanvas(canvasContainer.width(), canvasContainer.height());
-  // redrawCanvas(); // Redraw everything based on new size
-}
-
-// setup() function is called once when the program starts
 function setup() {
-  // place our canvas, making it fit our container
   canvasContainer = $("#canvas-container");
   let canvas = createCanvas(canvasContainer.width(), canvasContainer.height());
   canvas.parent("canvas-container");
-  // resize canvas is the page is resized
 
-  // create an instance of the class
-  myInstance = new MyClass("VALUE1", "VALUE2");
-
-  $(window).resize(function() {
-    resizeScreen();
-  });
-  resizeScreen();
-}
-
-// draw() function is called repeatedly, it's the main animation loop
-function draw() {
-  background(220);    
-  // call a method on the instance
-  myInstance.myMethod();
-
-  // Set up rotation for the rectangle
-  push(); // Save the current drawing context
-  translate(centerHorz, centerVert); // Move the origin to the rectangle's center
-  rotate(frameCount / 100.0); // Rotate by frameCount to animate the rotation
-  fill(234, 31, 81);
+  background(0);
+  
+  img.loadPixels();
   noStroke();
-  rect(-125, -125, 250, 250); // Draw the rectangle centered on the new origin
-  pop(); // Restore the original drawing context
-
-  // The text is not affected by the translate and rotate
-  fill(255);
-  textStyle(BOLD);
-  textSize(140);
-  text("p5*", centerHorz - 105, centerVert + 40);
+  
+  for(let i = 0; i < 256; i++) {
+    amplitudes0.push(0);
+    amplitudes2.push(0);
+    oscillators.push(new p5.Oscillator())
+    oscillators[i].pan(-1);
+    oscillators[i].phase(random() * 360);
+    oscillators2.push(new p5.Oscillator())
+    oscillators2[i].pan(1);
+    oscillators2[i].phase(random() * 360);
+  }
 }
 
-// mousePressed() function is called once after every time a mouse button is pressed
 function mousePressed() {
-    // code to run when mouse is pressed
+  for(let i = 0; i < 256; i++) {
+    oscillators[i].start();
+    oscillators2[i].start();
+  }
+}
+
+function draw() {
+  fill(0, 50);
+  rect(0, 0, 256, 256);
+  fill(255, 255, 255);
+  
+  for(let i = 0; i < 256; i++) {
+    amplitudes0[i] = 0;
+    amplitudes2[i] = 0;
+  }
+  
+  for(let i = 0; i < 4000 && index < img.pixels.length; i += 1) {
+    fill(img.pixels[index], img.pixels[index + 1], img.pixels[index + 2]);
+    rect(img.pixels[index], img.pixels[index + 2] , 1, 1);
+    let p_index = floor(index / 4);
+    rect((p_index % img.width) * 0.3, 256 + floor(p_index / img.width) * 0.3, 1, 1);
+    amplitudes0[img.pixels[index]] += 1;
+    amplitudes2[img.pixels[index + 2]] += 1;
+    index += 4;
+  }
+  
+  fill(255);
+  for(let i = 0; i < 256; i++) {
+    if(amplitudes2[i] > 255) {
+      amplitudes2[i] = 255;
+    }
+    oscillators[i].freq(100 + amplitudes2[i] * 2);
+    rect(amplitudes2[i], i, 1, 1);
+  }
+  for(let i = 0; i < 256; i++) {
+    if(amplitudes0[i] > 255) {
+      amplitudes0[i] = 255;
+    }
+    oscillators2[i].freq(100 + amplitudes0[i] * 2);
+    rect(i, amplitudes0[i], 1, 1);
+  }
 }
